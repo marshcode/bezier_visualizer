77c482de2bdb3d1a8f908035edac7a26e1bac48c
David Marsh <davemarsh@marshcode.com>
2013-04-10 01:54:23 +0000

    added output

--- tests/core/test_bezier.js
+++ tests/core/test_bezier.js
@@ -3,23 +3,23 @@ module("TestDim3");
 test("FullInit", function(){
 
 	var d = new Dim3(1,2,3);
-	equal(d.x, 1);
-	equal(d.y, 2);
-	equal(d.z, 3);
+	equal(d.x, 1, "Assert X Coordinate");
+	equal(d.y, 2, "Assert X Coordinate");
+	equal(d.z, 3, "Assert Z Coordinate");
 });
 
 test("EmptyInit", function(){
 
 	var d = new Dim3();
-	equal(d.x, 0);
-	equal(d.y, 0);
-	equal(d.z, 0);
+	equal(d.x, 0, "Assert X Coordinate");
+	equal(d.y, 0, "Assert Y Coordinate");
+	equal(d.z, 0, "Assert Z Coordinate");
 });
 
 test("PartialInit", function(){
 
 	var d = new Dim3(1,null,3);
-	equal(d.x, 1);
-	equal(d.y, 0);
-	equal(d.z, 3);
+	equal(d.x, 1, "Assert X Coordinate");
+	equal(d.y, 0, "Assert Y Coordinate");
+	equal(d.z, 3, "Assert Z Coordinate");
 });
