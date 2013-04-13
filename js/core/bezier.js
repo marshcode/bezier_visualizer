1fb74846f566d98c5d762f7647c8bc79d11dc97a
David Marsh <davemarsh@marshcode.com>
2013-04-13 14:51:36 +0000

    refactoring to make it more javascripty;

--- js/core/bezier.js
+++ js/core/bezier.js
@@ -13,9 +13,9 @@ function IllegalArgumentError(){};
 //require actual classes for these but there is no reason to get caught up in that now.
 
 function Dim3(x,y,z){
-	this.x = x ? x : 0
-	this.y = y ? y : 0
-	this.z = z ? z : 0
+	this.x = x || 0;
+	this.y = y || 0;
+	this.z = z || 0; 
 }
 
 /////////////////////////////////////////
