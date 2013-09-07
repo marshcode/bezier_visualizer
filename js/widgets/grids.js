eb9c518799929ff492b4d00dd17d81b86dc90f8b
David Marsh <davemarsh@marshcode.com>
2013-09-07 08:16:48 +0000

    added some nicer formatting.

--- js/widgets/grids.js
+++ js/widgets/grids.js
@@ -13,10 +13,11 @@ BEZIER.widgets.control_point_grid = function (storage, curve_name) {
 		minSpareRows: 1,
 		contextMenu: ['row_above', 'row_below', 'remove_row', 'hsep3', 'undo', 'redo'],
 		fillHandle: false,
-		colHeaders: ['X', 'Y', 'Z'], //, 'Highlight'],
+		colHeaders: ['INDEX', 'X', 'Y', 'Z'], //, 'Highlight'],
 	    columns: [
+	        {data: 'index', type: "numeric", allowInvalid: false, readOnly:true},
 	        {data: 'x', type: "numeric", allowInvalid: false},
-	        {data: 'z', type: "numeric", allowInvalid: false},
+	        {data: 'y', type: "numeric", allowInvalid: false},
 	        {data: 'z', type: "numeric", allowInvalid: false},
 	        //{data: 'highlight', type: 'checkbox', allowInvalid: false, readOnly: false}
 	    ],
@@ -25,9 +26,26 @@ BEZIER.widgets.control_point_grid = function (storage, curve_name) {
 
 		beforeChange: function (changes, source) {},
 
-		afterChange: function (changes, source) {}
+		afterChange: function (changes, source) {},
 
+	    cells: function (row, col, prop) {
+            var cellProperties = {};
+            cellProperties.renderer = function (instance, td, row, col, prop, value, cellProperties) {
+            	Handsontable.TextCell.renderer.apply(this, arguments);
+                if (col === 0) {
+                    td.style.fontWeight = 'bold';
+                    td.style.color = 'black';
+                    td.style.background = '#EEE';
+                }else{
+                	td.style.fontWeight = 'normal';
+                }
 
+            };
+		    return cellProperties;
+		}
+	
+		
+		
 	});
 	
 	var that = {
@@ -45,7 +63,8 @@ BEZIER.widgets.control_point_grid = function (storage, curve_name) {
 				
 				for (var i = 0; i < curve.num_points(); i++) {
 					pt = curve.get_point(i);
-					data.push({x: pt.x, 
+					data.push({index:i,
+							   x: pt.x, 
 							   y: pt.y, 
 							   z: pt.z});
 				}
