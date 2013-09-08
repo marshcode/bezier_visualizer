d5d960bd4868aff6db1e60419da9a48f62f3761d
David Marsh <davemarsh@marshcode.com>
2013-09-08 17:46:13 +0000

    append button.

--- js/widgets/grids.js
+++ js/widgets/grids.js
@@ -18,78 +18,85 @@ BEZIER.widgets.control_point_grid = function (storage, curve_name) {
 	    columns: [
 	        {data: 'x', type: "numeric", allowInvalid: false},
 	        {data: 'y', type: "numeric", allowInvalid: false},
-	        {data: 'z', type: "numeric", allowInvalid: false},
+	        {data: 'z', type: "numeric", allowInvalid: false}
 	        //{data: 'highlight', type: 'checkbox', allowInvalid: false, readOnly: false}
 	    ],
 
 		afterRemoveRow: function (index) {
-		  	if(!storage.has_curve(curve_name)){
-				return
-			}
-		  	
-		  	var curve = storage.get_curve(curve_name);
-		  	curve.remove_point(index);
-		  	storage.updated(curve_name);
-		},
+                if (!storage.has_curve(curve_name)) {
+				    return;
+	            }
+
+                var curve = storage.get_curve(curve_name);
+                curve.remove_point(index);
+                storage.updated(curve_name);
+		    },
 
 		beforeChange: function (changes, source) {
-		  	if(!changes){
-		  		return;
-		  	}
-	  	
-	  		$(changes).each( function(idx, change){
-	  		  	//typeof(change[3] == 'string' && change[3].length === 0
-	  			//redundant when new value is 0 but it catches all of the empty rows
-	  			if( !change[3] && change[1] != "highlight"){
-	  				changes[idx][3] = 0;
-	  			}
-	  		});
+            if (!changes) {
+                return;
+	        }
+
+            $(changes).each(function (idx, change) {
+                //typeof(change[3] == 'string' && change[3].length === 0
+                //redundant when new value is 0 but it catches all of the empty rows
+                if (!change[3] && change[1] !== "highlight") {
+                    changes[idx][3] = 0;
+                }
+            });
 			
 			
 		},
 
 		afterChange: function (changes, source) {
-		  	if(!changes){
-		  		return;
-		  	}
-		  	if(!storage.has_curve(curve_name)){
-				return
-			}
-		  	
+            if (!changes) {
+                return;
+            }
+            if (!storage.has_curve(curve_name)) {
+				return;
+            }
+
 			var curve = storage.get_curve(curve_name);
-		  	var row, prop, old_val, new_val;
-		  	var point;
-		  	$(changes).each(function(idx, change){
-		  		row = change[0]; prop = change[1]; old_val = change[2]; new_val = change[3];
-		  		point = curve.get_point(row);
-		  		
-		  		if(old_val === new_val){
-		  			return //nothing to do here
-		  		}
-		  		
-		  		if(prop === 'x'){
-		  			point.x = new_val;
-		  		}else if(prop === 'y'){
-		  			point.y = new_val;
-		  		}else if(prop === 'z'){
-		  			point.z = new_val;
-		  		}
-		  	});
-		  	
-		  	storage.updated(curve_name);
-		  	
+            var row, prop, old_val, new_val;
+            var point;
+            $(changes).each(function (idx, change) {
+                row = change[0]; prop = change[1]; old_val = change[2]; new_val = change[3];
+                point = curve.get_point(row);
+
+                if (old_val === new_val) {
+                    return; //nothing to do here
+                }
+
+                if (prop === 'x') {
+                    point.x = new_val;
+                } else if (prop === 'y') {
+                    point.y = new_val;
+                } else if (prop === 'z') {
+                    point.z = new_val;
+                }
+            });
+
+            storage.updated(curve_name);
+
 		},
 		
-		afterCreateRow: function(index){
-			if(!storage.has_curve(curve_name)){
-				return
+		afterCreateRow: function (index) {
+			if (!storage.has_curve(curve_name)) {
+				return;
 			}
 			
 			var curve = storage.get_curve(curve_name);
-			curve.insert_point(index, BEZIER.core.dim3(0, 0, 0));
+			var pt = BEZIER.core.dim3(0, 0, 0);
+			
+            if (curve.num_points() < index) {
+                curve.insert_point(index, pt);
+			} else {
+				curve.append_point(pt);
+			}
+			
 			storage.updated(curve_name);
 			
-		},
+		}
 		
 
 		
